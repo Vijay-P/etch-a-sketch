9f0be4e608edc47d67d45c4adf7115f19e987aca
Vijay Pillai <vijay.pillai397@gmail.com>
2018-03-27 23:08:34 -0400

    update again

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,28 +1,46 @@
 #define POTX A5 // x axis
 #define POTY A4 // y axis
-#define INP1 1  // button
+#define INP1 0  // button
+#define FSR  A3 // fsr
 
 unsigned long targetTime=0;
 const unsigned long interval=100;
-const int buttonPin = 0;
+unsigned long lastDebounceTime = 0;
 int buttonState = 0;
 int xPos = 0;
 int yPos = 0;
+int fsrValue = 0;
 
 void setup(){
-    pinMode(buttonPin, INPUT);
+    pinMode(INP1, INPUT);
     Serial.begin(115200);
 }
 
 void loop(){
-	if(millis()>=targetTime){
-		targetTime= millis()+interval;
-    xPos = analogRead(POTX);
-    yPos = analogRead(POTY);
-    Serial.println(String(String(xPos, DEC) + ',' + String(yPos, DEC)));
-	}
-    buttonState = digitalRead(buttonPin);
+    if(millis()>=targetTime){
+    	targetTime= millis()+interval;
+      xPos = analogRead(POTX);
+      yPos = analogRead(POTY);
+      Serial.println(String(String(xPos, DEC) + ',' + String(yPos, DEC)));
+    }
+    buttonState = digitalRead(INP1);
     if (buttonState == LOW) {
         Serial.println("rst");
     }
+
+    fsrValue = analogRead(FSR);
+    if(readFsr(fsrValue) == true){
+      Serial.println("color");
+    }
+
+}
+
+boolean readFsr(int fsrValue){
+  if (fsrValue > 200) {
+    if (millis()-lastDebounceTime > 200){
+       lastDebounceTime = millis();
+       return true;
+    }
+  }
+  return false;
 }
--- server.js
+++ server.js
@@ -34,12 +34,12 @@ serial.pipe(parser);
 parser.on('data', function(data) { // on data from the arduino
 	if (data == 'rst') { // if its the 'rst' string call reset
 		io.emit('reset');
+	} else if (data == 'color') {
+		var newColor = colorList[Math.floor(Math.random() * colorList.length)];
+		io.emit('color', newColor);
 	} else { // any other data we try to forward by spliting it
 		var transmitData = [data.split(',')[0], data.split(',')[1]];
 		io.emit('new-pos', transmitData);
-		var newColor = colorList[Math.floor(Math.random() * colorList.length)];
-		console.log(newColor);
-		io.emit('color', newColor);
 	}
 });
 //----------------------------------------------------------------------------//
